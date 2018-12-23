889dd0adb921cfab8f78b887524ed310aff63b7a
emirokanisildar <okan@fazlagida.com>
2018-12-23 09:50:54 +0300

    Boilerplate of the project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  )
+}
+
+export default App
