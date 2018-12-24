e2601cdd53315e9fb4ea47845d7f87f0c4fd3f03
emirokanisildar <okan@fazlagida.com>
2018-12-24 21:37:59 +0300

    Axios has added and tested the tastedive api

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import axios from 'axios'
 
-function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    axios.get('https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction').then((res) => {
+      console.log(res)
+    })
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
 }
 
 export default App
