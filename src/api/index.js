d5e831f927ae5c21de82e50a74983c9685709413
emirokanisildar <okan@fazlagida.com>
2019-05-04 16:08:24 +0300

    API_key retrieve using .env file finally

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
+import { API_KEY } from 'react-native-dotenv'
 
 const apiUrl = 'https://tastedive.com/api/similar?'
-const apiKey = '322571-TasteDiv-W3HBTXNH'
 
 const callApi = ({
   q = '', method = 'get', info = 1, type = '', limit = 10,
 }) => {
-  const url = `${apiUrl}k=${apiKey}&type=${type}&limit=${limit}&info=${info}&q=${q}`
+  const url = `${apiUrl}k=${API_KEY}&type=${type}&limit=${limit}&info=${info}&q=${q}`
   return new Promise(async (resolve, reject) => {
     try {
       const response = await axios({
